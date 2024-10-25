4b388f12736ba2e111b27e9496fc5ac6dfef36a3
Robert Spralja <robert.spralja@gmail.com>
2024-10-25 21:13:43 +0200

    infra: core - enabled cloudbuild api

--- infra/core/apis.tf
+++ infra/core/apis.tf
@@ -5,3 +5,7 @@ resource "google_project_service" "compute" {
 resource "google_project_service" "oslogin" {
   service = "oslogin.googleapis.com"
 }
+
+resource "google_project_service" "cloudbuild" {
+  service = "cloudbuild.googleapis.com"
+}
