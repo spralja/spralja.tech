6a44b3f6b04bc83fe0a43e435648f49c898759c5
Robert Spralja <robert.spralja@gmail.com>
2024-10-25 19:57:57 +0200

    infra - configured google provider

--- infra/core/vars.tf
+++ infra/core/vars.tf
@@ -7,3 +7,8 @@ variable "project" {
   description = "The project ID of a Google Cloud Platform project"
   type        = string
 }
+
+variable "region" {
+  description = "The default region to manage resources in"
+  type = string
+}
