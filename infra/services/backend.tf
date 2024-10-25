5b39798c339a32c58d417d97f9bb384381df7442
Robert Spralja <robert.spralja@gmail.com>
2024-10-25 22:48:34 +0200

    infra: services - init

---
+++ infra/services/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "gcs" {
+    bucket = "spralja-tech-tfstate"
+  }
+}
---
+++ infra/services/vars.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "The project ID of a Google Cloud Platform project"
+  type        = string
+}
+
+variable "region" {
+  description = "The default region to manage resources in"
+  type        = string
+}
